3a79fc9c716b6b772f16547be8ddeb5c72cdd3ef
Michael Lore <michael.lore@gmail.com>
2018-03-07 18:51:10 -0500

    Add validation

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -11,38 +12,40 @@ import (
 
 // site represents a web site in the view.
 type site struct {
-	Title string `json:"title"` // Title to display
-	URL   string `json:"url"`   // URL of the site
+	Title    string `json:"title"`    // Title to display
+	URL      string `json:"url"`      // URL of the site
+	Position int    `json:"position"` // Position of the panel
 }
 
 // process represents a OS process that commands can be sent to.
 type process struct {
-	Title   string   `json:"title"`         // Title to display
-	Command []string `json:"command"`       // Command to run to start the process
-	Dir     string   `json:"dir,omitempty"` // Working directory
-
-	cmd    *exec.Cmd
-	stdin  io.WriteCloser
-	stdout io.ReadCloser
-	stderr io.ReadCloser
-	wg     sync.WaitGroup
+	Title    string   `json:"title"`         // Title to display
+	Command  []string `json:"command"`       // Command to run to start the process
+	Dir      string   `json:"dir,omitempty"` // Working directory
+	Position int      `json:"position"`      // Position of the panel
+	cmd      *exec.Cmd
+	stdin    io.WriteCloser
+	stdout   io.ReadCloser
+	stderr   io.ReadCloser
+	wg       sync.WaitGroup
 }
 
 // step is a step of the demo that includes a command to run.
 type step struct {
 	Title     string `json:"title"`               // Title to display
 	Desc      string `json:"desc,omitempty"`      // Description to display (Markdown allowed)
-	ID        string `json:"id"`                  // ID of the step to operate on
+	ID        string `json:"id"`                  // ID of the process to operate on
 	Input     string `json:"input"`               // Command to send to process
 	Interrupt bool   `json:"interrupt,omitempty"` // Send to interrupt command
 }
 
 // config holds the sites, processes, and steps for the demo.
 type config struct {
-	Title     string              `json:"title"`               // Title to display
-	Sites     map[string]*site    `json:"sites,omitempty"`     // Sites to show
-	Processes map[string]*process `json:"processes,omitempty"` // Processes to start
-	Steps     []step              `json:"steps"`               // Steps to enable
+	Title         string              `json:"title"`               // Title to display
+	StepsPosition int                 `json:"stepsPosition"`       // Position of the steps panel
+	Sites         map[string]*site    `json:"sites,omitempty"`     // Sites to show
+	Processes     map[string]*process `json:"processes,omitempty"` // Processes to start
+	Steps         []step              `json:"steps"`               // Steps to enable
 }
 
 // readConfig loads a JSON file that stores a config.
@@ -53,6 +56,10 @@ func readConfig(file string) (*config, error) {
 	}
 	var c config
 	err = json.Unmarshal(b, &c)
+	if err != nil {
+		return nil, err
+	}
+	err = c.Validate()
 	if err != nil {
 		return nil, err
 	}
@@ -86,6 +93,34 @@ func (c *config) Wait() error {
 	}
 	if len(merr) > 0 {
 		return merr
+	}
+	return nil
+}
+
+// Validate checks for inconsistencies in the config.
+func (c *config) Validate() error {
+	var m multiError
+	for i := range c.Steps {
+		if _, ok := c.Processes[c.Steps[i].ID]; !ok && c.Steps[i].Input != "" {
+			m = append(m, fmt.Errorf("Step %d %q references missing process %q", i, c.Steps[i].Title, c.Steps[i].ID))
+		}
+	}
+	count := len(c.Processes) + len(c.Sites) + 1
+	for k, v := range c.Processes {
+		if v.Position < 0 || v.Position >= count {
+			m = append(m, fmt.Errorf("Position of process %q is out of bounds: %d", k, v.Position))
+		}
+	}
+	for k, v := range c.Sites {
+		if v.Position < 0 || v.Position >= count {
+			m = append(m, fmt.Errorf("Position of site %q is out of bounds: %d", k, v.Position))
+		}
+	}
+	if c.StepsPosition < 0 || c.StepsPosition >= count {
+		m = append(m, fmt.Errorf("Position of steps panel is out of bounds: %d", c.StepsPosition))
+	}
+	if len(m) > 0 {
+		return m
 	}
 	return nil
 }
